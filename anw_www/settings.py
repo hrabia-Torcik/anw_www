fc8378ffe89c33826aa2c5c405985092ef8fce79
hrabia-Torcik <dlafanudokanu@gmail.com>
2026-02-09 22:31:31 +0100

    Włączyłem debugi

--- anw_www/settings.py
+++ anw_www/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)sfrn=$$w681kfk32!jqezwh%iq1b0o6r6n0+k90wolgns&s(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
