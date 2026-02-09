558291f057ec87f91a4391d2677843ac375aad6a
hrabia-Torcik <dlafanudokanu@gmail.com>
2026-02-09 22:28:12 +0100

    Dodałem dopuszczenie dowolnego hosta.

--- anw_www/settings.py
+++ anw_www/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)sfrn=$$w681kfk32!jqezwh%iq1b0o6r6n0+k90wolgns&s(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
